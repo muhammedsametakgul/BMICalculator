fba0d63b60e2da7c228ae6aca1109f009738a983
Muhammed Samet Akgül <93324656+muhammedsametakgul@users.noreply.github.com>
2022-12-07 16:28:48 +0300

    Done Project

---
+++ app/src/main/java/com/sametakgul/bmi/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.sametakgul.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.sametakgul.bmi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnHesapla.setOnClickListener {
+            val Kilo=binding.etKilo.text.toString()
+            val Boy=binding.etBoy.text.toString()
+
+            val bmi=(Kilo.toFloat())/((Boy.toFloat()/100)*(Boy.toFloat()/100))
+            binding.txtsonuc.text=String.format("%.2f",bmi)
+
+        if(bmi<16)
+        {
+            binding.txtSonuc2.text="Aşırı Zayıf"
+
+        }
+        if(16<bmi && bmi<16.9)
+        {
+                binding.txtSonuc2.text="Ilımlı Zayıf"
+
+
+        }
+        if(17<bmi && bmi<18.4)
+        {
+            binding.txtSonuc2.text="Hafif Zayıf"
+
+        }
+        if(18.5<bmi && bmi<24.9)
+        {
+            binding.txtSonuc2.text="Normal"
+
+        }
+        if(25<bmi && bmi<29.9)
+        {
+            binding.txtSonuc2.text="Hafif Kilolu"
+
+        }
+        if(30<bmi )
+        {
+            binding.txtSonuc2.text="Obez"
+
+        }
+
+
+
+
+        }
+    }
+}
